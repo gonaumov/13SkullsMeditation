24a7b12634676c19c59da2ae8ebc960298882799
Georgi Naumov <gonaumov@gmail.com>
2015-02-13 22:04:34 +0200

    Finish the animation.

--- scripts/site.js
+++ scripts/site.js
@@ -1,8 +1,27 @@
 (function createSkulls(white) {
+    var usedSkulls = document.querySelectorAll('.skull');
+
+    [].forEach.call(usedSkulls, function(skull) {
+        skull.parentNode.removeChild(skull);
+    });
+
     var bigSkull = document.createElement("img");
     var left = (window.innerWidth / 2) - (bigSkull.width / 2);
     var tops = (window.innerHeight / 2) - (bigSkull.height / 2);
-    bigSkull.setAttribute("src", "images/whiteSkull.png");
+    var skullSrc, smallSkullSrc;
+    if(white) {
+        document.body.style.backgroundColor = "white";
+        skullSrc = "images/whiteSkull.png";
+        smallSkullSrc = "images/whiteSkullSmall.png";
+        white = false;
+    } else {
+        document.body.style.backgroundColor = "black";
+        skullSrc = "images/blackSkull.png";
+        smallSkullSrc = "images/blackSkullSmall.png";
+        white = true;
+    }
+
+    bigSkull.setAttribute("src", skullSrc);
 
     var steps = 360 / 12;
     var radius = 230;
@@ -12,7 +31,7 @@
             (function createCurrentSkull(i) {
                 if (i <= 360) {
                     var smallSkull = document.createElement("img");
-                    smallSkull.setAttribute("src", "images/whiteSkullSmall.png");
+                    smallSkull.setAttribute("src", smallSkullSrc);
                     var smallSkullHeight = smallSkull.height;
                     var smallSkullWidth = smallSkull.width;
                     var x = Math.floor(((left - (bigSkull.width / 2) + (smallSkullWidth / 2)) + radius * Math.cos(i * Math.PI / 180)));
@@ -40,6 +59,13 @@
         bigSkull.setAttribute("style", "position: absolute; left:" +
         (left - (bigSkull.width / 2)) + "px; top: " +
         (tops - (bigSkull.height / 2)) + "px;");
+        var animationEndHandler = function () {
+            createSkulls(white);
+        };
+
+        bigSkull.addEventListener("animationend", animationEndHandler);
+        bigSkull.addEventListener("webkitAnimationEnd", animationEndHandler);
+        bigSkull.addEventListener("oAnimationEnd", animationEndHandler);
         document.body.appendChild(bigSkull);
     });
 }(true));
